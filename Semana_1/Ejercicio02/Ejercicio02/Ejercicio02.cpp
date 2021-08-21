03644bffa13888419116a174a0ea27b9335760e2
tchambil <antony_epis@hotmail.com>
2021-08-20 22:59:29 -0500

    add enunciado eje02

--- Semana_1/Ejercicio02/Ejercicio02/Ejercicio02.cpp
+++ Semana_1/Ejercicio02/Ejercicio02/Ejercicio02.cpp
@@ -1,4 +1,16 @@
-  
+﻿  /*
+   * Elaborar un programa en entorno consola que permita almacenar las notas de “n” alumnos. El valor de “n” será ingresado por el usuario y las notas deben ser generadas en forma aleatoria y deben estar 0 y 20.
+El programa debe realizar lo siguiente:
+	Generar las notas
+	Listar en pantalla las notas
+	Mostrar la mayor nota
+	Mostrar la menor nota
+	Mostrar la nota promedio
+	Mostrar el porcentaje de aprobados y desaprobados
+	Ordenar el arreglo en forma descendente
+En la solución considere un menú de opciones para cada punto descrito y la solución del programa debe contemplar funciones, punteros y arreglos dinámicos.
+
+   */
 #include "Menu.h"
 
 int main()
