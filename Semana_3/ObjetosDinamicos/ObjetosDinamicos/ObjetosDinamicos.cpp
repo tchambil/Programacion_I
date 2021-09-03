8be5cf4063ae69a0dddf022045e83d851e59f942
tchambil <antony_epis@hotmail.com>
2021-09-03 11:15:47 -0500

    add Arreglo dinámico de objetos

--- Semana_3/ObjetosDinamicos/ObjetosDinamicos/ObjetosDinamicos.cpp
+++ Semana_3/ObjetosDinamicos/ObjetosDinamicos/ObjetosDinamicos.cpp
@@ -16,7 +16,7 @@ int main()
 	cout << &ptrP1 << endl;
 	*/
 	//Memoria dinamica (asignación dinámica de memoria 	)
-
+	/*
 	CPunto2D* ptrP2 = new CPunto2D(55,22);
 	CPunto2D* ptrP3 = new CPunto2D(10, 6);
 	cout << "--ptr2---" << endl;
@@ -28,10 +28,43 @@ int main()
 	cout << ptrP3->getY() << endl;
 	cout << &ptrP3 << endl;
 	delete ptrP2;
+	delete ptrP3;
+	*/
+	//Arreglo dinámico de objetos
+	CPunto2D* ptrPuntos = new CPunto2D[5];
+	ptrPuntos[0] = CPunto2D(5, 4);
+	ptrPuntos[1] = CPunto2D(6, 2);
+	ptrPuntos[2] = CPunto2D(1, 1);
+	ptrPuntos[3] = CPunto2D(3, 1);
+	ptrPuntos[4] = CPunto2D(2, 2);
+
+	for (auto i = 0; i < 5; ++i) {
+		cout << ptrPuntos[i].getX() << ","
+			<< (*(ptrPuntos + i)).getY() << endl;
+	}
+	delete ptrPuntos;
+	ptrPuntos = nullptr;
+	//otra forma
+	CPunto2D* ptrPuntos2 = new CPunto2D[5]
+	{ CPunto2D(5, 4),
+		CPunto2D(6, 2),
+		 CPunto2D(1, 1),
+		CPunto2D(3, 1),
+		 CPunto2D(2, 2)
+	};
+
+	for (auto i = 0; i < 5; ++i) {
+		cout << ptrPuntos2[i].getX() << ","
+			<< (*(ptrPuntos2 + i)).getY() << endl;
+	}
+	delete ptrPuntos2;
+	ptrPuntos2 = nullptr;
 
 
 
 
-	 
+
+
+
 }
 
